746fbcb6d16d8cd863d54d9fac47f28dadba8152
Douglas Pina <aysllan_douglas@hotmail.com>
2022-02-12 23:04:38 -0300

    ola mundo
    retirada ola mundo

--- src/salao/Salao.js
+++ src/salao/Salao.js
@@ -48,8 +48,7 @@ function Salao() {
                 É um conjunto de formas, de curvas e contracurvas onde, com tinta 
                 vermelha de paixão, se escreve o conto da criação.
                 Beleza de mulher é arte, é poesia, são todas as notas da mais bela melodia, 
-                são encantos de mistério e loucura onde se guarda a origem da vida. 
-                ola mundo!!
+                são encantos de mistério e loucura onde se guarda a origem da vida.
                 <br />
                 Nós da <strong>BELLA'S Hair Salon</strong> trabalhamos todos os dias, para que cada mulher se sinta ÚNICA.
                 </p>
